b3bbf9a7eecf88c17b71c319e5d78bf87fccaafc
Odyseus <venusyadonis@gmail.com>
2020-03-08 09:13:07 -0300

    Updated manual page and bump version

--- AppData/BootstrapThemesGeneratorApp/__init__.py
+++ AppData/BootstrapThemesGeneratorApp/__init__.py
@@ -3,7 +3,7 @@
 __status__ = "Stable"
 __appname__ = "Bootstrap Themes Generator"
 __appdescription__ = "CLI utility to assist in the creation of Bootstrap themes."
-__version__ = "2020-03-02 22:46:22.412"
+__version__ = "2020-03-08 09:10:52.454"
 
 
 if __name__ == "__main__":
